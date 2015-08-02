02169353ebb0fb2b4414a2881af89e9b03f00789
charlyn23 <charlyn.buchanan@gmail.com>
2015-08-02 11:45:01 -0400

    Setting change and prefs zipcode validation added

--- OMG/app/src/main/java/charlyn23/c4q/nyc/omg/MainActivity.java
+++ OMG/app/src/main/java/charlyn23/c4q/nyc/omg/MainActivity.java
@@ -1,17 +1,12 @@
 package charlyn23.c4q.nyc.omg;
 
 import android.content.Intent;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.widget.Toast;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -30,23 +25,113 @@ import charlyn23.c4q.nyc.omg.model.Location;
 import charlyn23.c4q.nyc.omg.model.Offices;
 import charlyn23.c4q.nyc.omg.model.Program;
 import charlyn23.c4q.nyc.omg.model.SearchResult;
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
-    public final String AB_URL = "https://searchbertha-hrd.appspot.com/_ah/api/search/v1/zipcodes/10101/programs?api_key=b0f6c6a6a8be355fc04be76ab3f0c5e6&serviceTag=immediate%20safety";
+//    public final String AB_URL = "https://searchbertha-hrd.appspot.com/_ah/api/search/v1/zipcodes/10101/programs?api_key=b0f6c6a6a8be355fc04be76ab3f0c5e6&serviceTag=immediate%20safety";
+
+    final static String url1 = "https://searchbertha-hrd.appspot.com/_ah/api/search/v1/zipcodes/";
+     String url2;
+    int zipCode;
+     String AB_URL;
+    Button not_safe_button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent intent= new Intent(this, MappingImmediateHelp.class);
-        startActivity(intent);
+        SettingsActivity settingsActivity = new SettingsActivity();
 
 
+        Button not_safe_button = (Button)findViewById(R.id.not_safe_button);
+        Button food_button = (Button)findViewById(R.id.food_button);
+        Button hurt_button = (Button)findViewById(R.id.hurt_button);
+        Button shelter_button= (Button)findViewById(R.id.shelter_button);
+        Button mental_button= (Button)findViewById(R.id.mental_button);
+        Button money_button= (Button)findViewById(R.id.money_button);
+        Button pet_help_button= (Button)findViewById(R.id.pet_help_button);
+        Button missing_person_button= (Button)findViewById(R.id.missing_person_button);
+
+        View.OnClickListener notSafeListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData("https://searchbertha-hrd.appspot.com/_ah/api/search/v1/zipcodes/", zipCode, "/programs?api_key=b0f6c6a6a8be355fc04be76ab3f0c5e6&serviceTag=immediate%20safety");
+            }
+        };
+        not_safe_button.setOnClickListener(notSafeListener);
+
+        View.OnClickListener foodListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData(url1, zipCode, "/programs?api_key=b0f6c6a6a8be355fc04be76ab3f0c5e6&serviceTag=emergency%20food");
+            }
+        };
+        food_button.setOnClickListener(foodListener);
+
+        View.OnClickListener hurtListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                getData(url1, zipCode, url2);
+            }
+        };
+
+        View.OnClickListener missingPersonListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData(url1, zipCode, "/programs?api_key=b0f6c6a6a8be355fc04be76ab3f0c5e6&serviceTag=help%20find%20missing%20persons");
+            }
+        };
+        missing_person_button.setOnClickListener(missingPersonListener);
+
+        View.OnClickListener shelterListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData(url1, zipCode, "/programs?api_key=b0f6c6a6a8be355fc04be76ab3f0c5e6&serviceTag=emergency%20shelter");
+            }
+        };
+        shelter_button.setOnClickListener(shelterListener);
+
+        View.OnClickListener emotionalListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData(url1, zipCode, "/programs?api_key=b0f6c6a6a8be355fc04be76ab3f0c5e6&serviceTag=psychiatric%20emergency%20services");
+            }
+        };
+        mental_button.setOnClickListener(emotionalListener);
+
+        View.OnClickListener moneyListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData(url1, zipCode, "/programs?api_key=b0f6c6a6a8be355fc04be76ab3f0c5e6&serviceTag=emergency%20payments");
+            }
+        };
+        money_button.setOnClickListener(moneyListener);
+
+//        Intent intent= new Intent(this, MappingImmediateHelp.class);
+//        startActivity(intent);
+
+
+
+    }
+
+    public void getData(String url1, int zipcode, String url2) {
+        SharedPreferences pref = getSharedPreferences("MyPrefsFile", MODE_PRIVATE);
+        SettingsActivity settingsActivity = new SettingsActivity();
+        zipCode = pref.getInt("user zipcode", -1);
+
+        if (zipCode == -1) {
+            Toast.makeText(this, "Please enter your zip code in Settings.", Toast.LENGTH_SHORT);
+        }
+        else if (((zipCode <= 10001) || (zipCode >= 11697))){
+            Toast.makeText(this, "Please enter a valid NYC zip code in Settings", Toast.LENGTH_SHORT).show();
+        }
+
+
+        Log.i("Zip Code: " , String.valueOf(zipCode));
+//        String url2 = "/programs?api_key=b0f6c6a6a8be355fc04be76ab3f0c5e6&serviceTag=emergency%20food";
+        String AB_URL = (url1 + zipCode + url2);
         RequestQueue queue = Volley.newRequestQueue(this);
         StringRequest stringRequest = new StringRequest(Request.Method.GET, AB_URL,
                 new Response.Listener<String>() {
@@ -54,7 +139,7 @@ public class MainActivity extends ActionBarActivity {
                     @Override
                     public void onResponse(String response) {
                         SearchResult searchResult = new Gson().fromJson(response, SearchResult.class);
-                        Log.i("Results : " , searchResult.getPrograms().toString());
+                        Log.i("Results : ", searchResult.getPrograms().toString());
 
 
                         List<Program> programs = searchResult.getPrograms();
@@ -63,9 +148,12 @@ public class MainActivity extends ActionBarActivity {
 
                         for (Program program : searchResult.getPrograms()) {
                             Log.i("Results : ", program.getName());
+                            Toast.makeText(getApplicationContext(), program.getName(), Toast.LENGTH_SHORT).show();
                             for (ContactInfo contactInfo : program.getNext_steps()) {
                                 if (contactInfo.getChannel().equals("phone")) {
                                     Log.i("Phone Number : ", contactInfo.getContact());
+//                                    Toast.makeText(getApplicationContext(), contactInfo.getContact(), Toast.LENGTH_SHORT).show();
+
                                 }
                             }
                             for (Offices offices : program.getOffices()) {
@@ -90,11 +178,10 @@ public class MainActivity extends ActionBarActivity {
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(), "That didn't work!", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getApplicationContext(), "Please enter a valid zip code", Toast.LENGTH_SHORT).show();
             }
         });
         queue.add(stringRequest);
-
     }
 
     public void settingsOnClick(View view) {
---
+++ OMG/app/src/main/java/charlyn23/c4q/nyc/omg/StringBuilder.java
@@ -0,0 +1,17 @@
+package charlyn23.c4q.nyc.omg;
+
+/**
+ * Created by charlynbuchanan on 8/1/15.
+ */
+public class StringBuilder {
+
+    final static String url1 = "https://searchbertha-hrd.appspot.com/_ah/api/search/v1/zipcodes/";
+    final static String url2 = "/programs?api_key=b0f6c6a6a8be355fc04be76ab3f0c5e6&serviceTag=immediate%20safety";
+    int zipCode;
+    StringBuilder stringBuilder = new StringBuilder();
+    String line = "";
+
+    String jsonURL = (url1 + zipCode + url2);
+
+
+}
