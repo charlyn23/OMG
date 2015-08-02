dc02597ec298d47637014d4dbc1f42bc99045d87
jgarcia162 <jogarcia809@yahoo.com>
2015-08-02 13:03:26 -0400

    removed logs

--- OMG/app/src/main/java/charlyn23/c4q/nyc/omg/MainActivity.java
+++ OMG/app/src/main/java/charlyn23/c4q/nyc/omg/MainActivity.java
@@ -8,7 +8,6 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.Toast;
-import android.util.Log;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -16,8 +15,6 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
-
-import java.net.URL;
 import java.util.List;
 import charlyn23.c4q.nyc.omg.db.ResourcesHelper;
 import charlyn23.c4q.nyc.omg.model.ContactInfo;
@@ -43,6 +40,9 @@ public class MainActivity extends ActionBarActivity {
     public final int EMOTIONAL_DB_ID = 6;
     private final String MONEY_URL = "https://searchbertha-hrd.appspot.com/_ah/api/search/v1/zipcodes/10101/programs?api_key=b0f6c6a6a8be355fc04be76ab3f0c5e6&serviceTag=emergency%20payments";
     public final int MONEY_DB_ID = 7;
+
+    private String table;
+
 
     ResourcesHelper helper = ResourcesHelper.getInstance(getApplicationContext());
     SQLiteDatabase mDatabase = helper.getWritableDatabase();
@@ -79,12 +79,9 @@ public class MainActivity extends ActionBarActivity {
                     @Override
                     public void onResponse(String response) {
                         SearchResult searchResult = new Gson().fromJson(response, SearchResult.class);
-                        Log.i("Results : ", searchResult.getPrograms().toString());
 
                         List<Program> programs = searchResult.getPrograms();
-                        Log.i("List: ", programs.toString());
 
-                         String table;
                         if(url.equalsIgnoreCase(IMMEDIATE_SAFETY_URL))
                             table = "IMMEDIATE SAFETY";
                         else if(url.equalsIgnoreCase(FOOD_URL))
@@ -102,30 +99,19 @@ public class MainActivity extends ActionBarActivity {
                         else
                             table = "";
 
-
                         for (Program program : searchResult.getPrograms()) {
-
-
-                            Log.i("Results : ", program.getName());
                             for (ContactInfo contactInfo : program.getNext_steps()) {
                                 String phoneNumber;
                                 if (contactInfo.getChannel().equals("phone")) {
                                     //ADDING PHONE NUMBER TO DB
                                     phoneNumber = contactInfo.getContact();
-                                    Log.i("Phone Number : ", contactInfo.getContact());
                                 }
                                 else{
                                     phoneNumber = "Unavailable";
                                 }
 
                             for (Offices offices : program.getOffices()) {
-
-                                Log.i("Address: ", offices.getAddress1());
-
                                 Location location = offices.getLocation();
-                                Log.i("Latitude : ", String.valueOf(location.getLatitude()));
-                                Log.i("Longitude : ", String.valueOf(location.getLongitude()));
-
                                 Hours hours = offices.getHours();
 
                                 String hoursString = "Monday: " + hours.getMonday_start() + " - " + hours.getMonday_finish() + "\n" +
@@ -136,18 +122,14 @@ public class MainActivity extends ActionBarActivity {
                                         "Saturday: " + hours.getSaturday_start() + " - " + hours.getSaturday_finish() + "\n" +
                                         "Sunday:" + hours.getSunday_start() + " - " + hours.getSunday_finish();
 
-                                Log.i("Monday Hours: ", String.valueOf(hours.getMonday_start()) + " - " + String.valueOf(hours.getMonday_finish()));
-                                Log.i("Tuesday Hours: ", String.valueOf(hours.getThursday_start()) + " - " + String.valueOf(hours.getTuesday_finish()));
-                                Log.i("Wednesday Hours: ", String.valueOf(hours.getWednesday_start()) + " - " + String.valueOf(hours.getWednesday_finish()));
-                                Log.i("Thursday Hours: ", String.valueOf(hours.getThursday_start()) + " - " + String.valueOf(hours.getThursday_finish()));
-                                Log.i("Friday Hours: ", String.valueOf(hours.getFriday_start()) + " - " + String.valueOf(hours.getFriday_finish()));
-                                Log.i("Saturday Hours: ", String.valueOf(hours.getSaturday_start()) + " - " + String.valueOf(hours.getSaturday_finish()));
-                                Log.i("Sunday Hours: ", String.valueOf(hours.getSunday_start() + " - " + String.valueOf(hours.getSunday_finish())));
-
-
-                                mDatabase.beginTransaction();
-                            helper.insertRow(program.getName(),offices.getAddress1(),phoneNumber,hoursString,table);
-                                mDatabase.endTransaction();
+                                try {
+                                    mDatabase.beginTransaction();
+                                    helper.insertRow(program.getName(), offices.getAddress1(), phoneNumber, hoursString, table);
+                                    mDatabase.setTransactionSuccessful();
+                                }
+                                finally{
+                                    mDatabase.endTransaction();
+                                }
                             }
                             }
                         }
@@ -159,7 +141,6 @@ public class MainActivity extends ActionBarActivity {
             }
         });
         queue.add(stringRequest);
-
     }
 
     public void settingsOnClick(View view) {
