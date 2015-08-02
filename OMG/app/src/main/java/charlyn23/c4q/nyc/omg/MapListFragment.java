c2bf082a5c4c9312750a45944a802ef4ced40c3f
MadelynTav <Madelyntav@gmail.com>
2015-08-02 12:01:42 -0400

    all info in listView

--- OMG/app/src/main/java/charlyn23/c4q/nyc/omg/MapListFragment.java
+++ OMG/app/src/main/java/charlyn23/c4q/nyc/omg/MapListFragment.java
@@ -1,11 +1,20 @@
 package charlyn23.c4q.nyc.omg;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import charlyn23.c4q.nyc.omg.model.Program;
 
 
 /**
@@ -13,18 +22,76 @@ import android.view.ViewGroup;
  */
 public class MapListFragment extends android.support.v4.app.Fragment {
 
-
+    ListView listView;
+    List<Program> list= new ArrayList<>();
+            //= new ArrayList<Program>();
+    ListAdapter adapter;
     public MapListFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map_list, container, false);
+
+        final View fragmentView= inflater.inflate(R.layout.fragment_map_list, container, false);
+
+        //rowView=getActivity().getLayoutInflater().inflate(R.layout.custom_location_list_view_item, null);
+        listView=(ListView) fragmentView.findViewById(R.id.listView);
+        adapter=new ListAdapter(getActivity(),R.id.location_name,list);
+        listView.setAdapter(adapter);
+
+
+        return fragmentView;
     }
 
+    public void updateData(List<Program> eventDataList) {
+        adapter.clear();
+        adapter.addAll(eventDataList);
+       // list.addAll(eventDataList);
+        adapter.notifyDataSetChanged();
+    }
+    private class ListAdapter extends ArrayAdapter {
+        public ListAdapter(Context context, int resource, List<Program> items) {
+            super(context, resource, items);
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View rowView = getActivity().getLayoutInflater().inflate(R.layout.custom_location_list_view_item, null);
+                String name = list.get(position).getName();
+
+                String address = list.get(position).getOffices().get(0).getAddress1();
+
+                TextView nameView = (TextView) rowView.findViewById(R.id.location_name);
+                nameView.setText(name);
+                TextView addressView = (TextView) rowView.findViewById(R.id.location_address_field);
+                addressView.setText(address);
+
+
+            String phoneNumber = list.get(position).getNext_steps().get(0).getContact();
+           TextView phoneNumberView = (TextView) rowView.findViewById(R.id.location_phone_number);
+            phoneNumberView.setText(phoneNumber);
+            String hours = "Monday: " + list.get(position).getOffices().get(0).getHours().getMonday_start() + " - " + list.get(position).getOffices().get(0).getHours().getMonday_finish() + "\nTuesday: " + list.get(position).getOffices().get(0).getHours().getTuesday_start() + " - " + list.get(position).getOffices().get(0).getHours().getTuesday_finish() + "\nWednesday: " + list.get(position).getOffices().get(0).getHours().getWednesday_start() + " - " + list.get(position).getOffices().get(0).getHours().getWednesday_finish() + "\nThursday: " + list.get(position).getOffices().get(0).getHours().getThursday_start() + " - " + list.get(position).getOffices().get(0).getHours().getThursday_finish() + "\nFriday: " + list.get(position).getOffices().get(0).getHours().getFriday_start() + " - " + list.get(position).getOffices().get(0).getHours().getFriday_finish();
+
+//            if (!list.get(position).getOffices().get(position).getHours().getSaturday_start().equals("")){
+//
+//                hours += "\nSaturday: "+
+//            list.get(position).getOffices().get(position).getHours().getSaturday_start() + " - " + list.get(position).getOffices().get(position).getHours().getSaturday_finish();
+//        }
+//                   if(!list.get(position).getOffices().get(position).getHours().getSaturday_start().equals( ""))
+//
+//        {
+//            hours += "\nSunday:" + list.get(position).getOffices().get(position).getHours().getSunday_start() + "-" + list.get(position).getOffices().get(position).getHours().getSunday_finish();
+//        }
+            TextView hoursView=(TextView) rowView.findViewById(R.id.location_hours);
+            hoursView.setText(hours);
+
+            return rowView;
+
+        }
+    }
 
 }
+
--- OMG/app/src/main/java/charlyn23/c4q/nyc/omg/MappingImmediateHelp.java
+++ OMG/app/src/main/java/charlyn23/c4q/nyc/omg/MappingImmediateHelp.java
@@ -9,6 +9,16 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -16,31 +26,86 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+
+import charlyn23.c4q.nyc.omg.model.ContactInfo;
+import charlyn23.c4q.nyc.omg.model.Hours;
+import charlyn23.c4q.nyc.omg.model.Location;
+import charlyn23.c4q.nyc.omg.model.Offices;
+import charlyn23.c4q.nyc.omg.model.Program;
+import charlyn23.c4q.nyc.omg.model.SearchResult;
 
 
 public class MappingImmediateHelp extends ActionBarActivity {
-
+    public final String AB_URL = "https://searchbertha-hrd.appspot.com/_ah/api/search/v1/zipcodes/10101/programs?api_key=b0f6c6a6a8be355fc04be76ab3f0c5e6&serviceTag=emergency%20food";
     ViewPager viewPager;
     MapFragment mapFragment;
     MapListFragment mapListFragment;
+    List<Program> programs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mapping_immediate_help);
-        mapFragment= new MapFragment();
-        mapListFragment= new MapListFragment();
 
-        viewPager=(ViewPager) findViewById(R.id.pager);
+        setContentView(R.layout.activity_mapping_immediate_help);
+        mapFragment = new MapFragment();
+        mapListFragment = new MapListFragment();
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
 
-        if(isNetworkAvailable()){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, AB_URL,
+                new Response.Listener<String>() {
 
-        }
-        else{
-            getInfoFromCache();
-        }
+                    @Override
+                    public void onResponse(String response) {
+                        SearchResult searchResult = new Gson().fromJson(response, SearchResult.class);
+                        Log.i("Results : ", searchResult.getPrograms().toString());
+
+
+                        programs = searchResult.getPrograms();
+                        Log.i("List: ", programs.toString());
+
+                        for (Program program : searchResult.getPrograms()) {
+                            Log.i("Results : ", program.getName());
+                            for (ContactInfo contactInfo : program.getNext_steps()) {
+                                if (contactInfo.getChannel().equals("phone")) {
+                                    Log.i("Phone Number : ", contactInfo.getContact());
+                                }
+                            }
+                            for (Offices offices : program.getOffices()) {
+                                Log.i("Address: ", offices.getAddress1());
+
+                                Location location = offices.getLocation();
+                                Log.i("Latitude : ", String.valueOf(location.getLatitude()));
+                                Log.i("Longitude : ", String.valueOf(location.getLongitude()));
+
+                                Hours hours = offices.getHours();
+                                Log.i("Monday Hours: ", String.valueOf(hours.getMonday_start()) + " - " + String.valueOf(hours.getMonday_finish()));
+                                Log.i("Tuesday Hours: ", String.valueOf(hours.getThursday_start()) + " - " + String.valueOf(hours.getTuesday_finish()));
+                                Log.i("Wednesday Hours: ", String.valueOf(hours.getWednesday_start()) + " - " + String.valueOf(hours.getWednesday_finish()));
+                                Log.i("Thursday Hours: ", String.valueOf(hours.getThursday_start()) + " - " + String.valueOf(hours.getThursday_finish()));
+                                Log.i("Friday Hours: ", String.valueOf(hours.getFriday_start()) + " - " + String.valueOf(hours.getFriday_finish()));
+                                Log.i("Saturday Hours: ", String.valueOf(hours.getSaturday_start()) + " - " + String.valueOf(hours.getSaturday_finish()));
+                                Log.i("Sunday Hours: ", String.valueOf(hours.getSunday_start() + " - " + String.valueOf(hours.getSunday_finish())));
+
+                            }
+                        }
+
+                        mapListFragment.updateData(programs);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "That didn't work!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        queue.add(stringRequest);
+        getInfoFromCache();
     }
+
     public void saveInfoToCache(){
         File file;
         FileOutputStream outputStream;
